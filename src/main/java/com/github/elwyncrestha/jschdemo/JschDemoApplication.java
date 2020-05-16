b287b08a1db58598f0815dd1dc91291c844721c7
Elvin Shrestha <elwyncrestha@gmail.com>
2020-05-16 09:49:07 +0545

    Initial Commit

---
+++ src/main/java/com/github/elwyncrestha/jschdemo/JschDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.elwyncrestha.jschdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JschDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JschDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/elwyncrestha/jschdemo/JschDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elwyncrestha.jschdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JschDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
